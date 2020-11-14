06deec176c4c005992877abc6971906026532edd
Pholith <vincent22062@gmail.com>
2020-11-14 16:33:58 +0100

    Fix bug and add ptions

--- SuperSpeed/SuperSpeed.lua
+++ SuperSpeed/SuperSpeed.lua
@@ -1,14 +1,25 @@
+local baseSpeed = ModVariable.GetVariableForObjectAsFloat("FarmerPlayer", "BaseDelay");
+local speedMultiplier = 10;
+
 function SteamDetails() 
-    ModBase.SetSteamWorkshopDetails("SuperSpeed", "Your player is now super fast", {"speed", "super"}, "SuperSpeed.png")
+    ModBase.SetSteamWorkshopDetails("SuperSpeed", "Your player is now super fast", {"speed", "movement"}, "SuperSpeed.png")
 end
 -- Used near exclusively for Steam Workshop and Mod information
 
-
 function BeforeLoad()
-    ModVariable.SetVariableForObjectAsFloat("FarmerPlayer", "BaseDelay", -2)
+    ModVariable.SetVariableForObjectAsFloat("FarmerPlayer", "BaseDelay", baseSpeed / speedMultiplier)
 
 end
 -- Initial load function by game
+
+function Expose() 
+    ModBase.ExposeVariable("Speed Multiplier", speedMultiplier, SpeedCallback, 1, 30)
+end 
+function SpeedCallback( param )
+    speedMultiplier = param
+end
+
+
 
 function AfterLoad()
 
